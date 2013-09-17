7818c06e8a59777f9f2e1794c4de556342f9da77
Justin Walgran <jwalgran@azavea.com>
2013-09-17 17:38:44 -0400

    1.2.11
    According the the following post there was a signing change between
    log4net versions 1.2.10 and 1.2.11 which caused problems
    
    http://devlicio.us/blogs/krzysztof_kozmic/archive/2012/01/31/how-to-force-nuget-not-to-update-log4net-to-1-2-11.aspx
    
    I updated to the (hopefully) fully compatible 1.2.11

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.10.*")]
-[assembly: AssemblyFileVersion("2.1.10")]
+[assembly: AssemblyVersion("2.1.11.*")]
+[assembly: AssemblyFileVersion("2.1.11")]
 // Used for nuget packaging
-[assembly: AssemblyInformationalVersion("2.1.10")]
+[assembly: AssemblyInformationalVersion("2.1.11")]
